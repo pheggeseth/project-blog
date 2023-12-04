bc3a5130c159e6b581c2088a0eb51de4f045031e
Paul Heggeseth <pheggeseth@launchdarkly.com>
2023-12-04 16:25:26 -0600

    exercise 7

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Rss, Sun, Moon } from 'react-feather';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { Rss, Sun, Moon } from "react-feather";
+import Cookie from "js-cookie";
 
-import Logo from '@/components/Logo';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import Logo from "@/components/Logo";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
+import { DARK_TOKENS, LIGHT_TOKENS } from "@/constants";
 
-function Header({ theme, className, ...delegated }) {
+function Header({ theme: initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -21,18 +23,27 @@ function Header({ theme, className, ...delegated }) {
             size="1.5rem"
             style={{
               // Optical alignment
-              transform: 'translate(2px, -2px)',
+              transform: "translate(2px, -2px)",
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+        <button
+          className={styles.action}
+          onClick={() => {
+            const nextTheme = theme === "light" ? "dark" : "light";
+            setTheme(nextTheme);
+            Cookie.set("color-theme", nextTheme, { expires: 1000 });
+            const root = document.documentElement;
+            const tokens = nextTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+            root.setAttribute("data-color-theme", nextTheme);
+            Object.entries(tokens).forEach(([key, value]) => {
+              root.style.setProperty(key, value);
+            });
+          }}
+        >
+          {theme === "light" ? <Moon size="1.5rem" /> : <Sun size="1.5rem" />}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
